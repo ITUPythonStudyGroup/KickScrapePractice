df06cdf5a9f1e461132ecac8b1d8f035a3516569
Adrian Brink <adrianbrink@users.noreply.github.com>
2015-10-21 19:00:45 +0200

    initial database setup

--- adrianbrink/scraper.py
+++ adrianbrink/scraper.py
@@ -1,22 +1,24 @@
-import urllib.request
-from bs4 import BeautifulSoup
+import rethinkdb as r
+import json
+import requests
 
-urlKs = 'https://www.kickstarter.com/projects/agfa/agfa-and-something-weird'
-html = urllib.request.urlopen(urlKs).read()
-soup = BeautifulSoup(html, 'html.parser')
-title = soup.find_all("div", class_="NS_projects__header")[0].h2.a.text
-datas = soup.find_all("data")
-projectId = datas[0]['class']
-temp = soup.find_all("div", class_="projectId")[0]
-temp = soup.find_all("div", class_="ksr_page_timer poll stat")[0].contents[0]
-temp1 = temp.text
+statusCode = 200
+page = 1
 
-backers = datas[0].text
-pledged = datas[1].text
-comments = datas[2].text
-print(datas)
-print('title:\t\t', title)
-print('backers:\t', backers)
-print('pledged:\t', pledged)
-print('comments:\t', comments)
-print(temp1)
+# Scraping stuff
+
+while statusCode == 200:
+    response = requests.get("https://www.kickstarter.com/projects/search.json?search=&term=&page=" + page.__str__())
+    text = response.text
+
+    jsonParsed = json.loads(text)
+    print(json.dumps(jsonParsed, indent=4))
+
+    statusCode = response.status_code
+    page += 1;
+    statusCode = 300
+
+# Database stuff
+conn = r.connect("52.28.17.23", 28015, db='kickstarter')
+r.table("projects").insert(jsonParsed).run(conn)
+
