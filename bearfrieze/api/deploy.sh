2b6b60e6a3b24c49b39e911a64ea0a8bb3a95518
Bjørn Friese <bearfrieze@gmail.com>
2015-10-24 17:50:31 +0200

    API deployed on Blackbeard

--- bearfrieze/api/deploy.sh
+++ bearfrieze/api/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-NAME=kick-api
+NAME=registry.blackbeard.io/bearfrieze/kick-api
 docker build -t $NAME .
-docker stop $NAME
-docker rm $NAME
-docker run -p 80:80 -d --restart=always --name $NAME $NAME
+docker login registry.blackbeard.io
+docker push $NAME
